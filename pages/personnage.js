1d1c32cb6f03475c9bd1f5003258f96732835f76
kimleegalarneau <kimlee.ny@hotmail.com>
2020-09-16 17:42:52 -0400

    page personnage

---
+++ pages/personnage.js
@@ -0,0 +1,11 @@
+export default function Personnages() {
+    return (
+        <div>
+            <h1>Toute une créature !</h1>
+            <a>Tous les personnages sont tous unique</a>
+            <MyButton color="blue" shadowSize={2}>
+                Générer votre personnage ici
+            </MyButton>
+        </div>
+    )
+}
