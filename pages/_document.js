bfc6ff82bf7bc53f0ef8bb55fb3c5fa2491ab262
Olivier Fortier <olivier_fortier@outlook.com>
2020-09-18 21:03:41 -0400

    ajout de méta information pour SEO

---
+++ pages/_document.js
@@ -0,0 +1,29 @@
+  
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+/* NE PAS TOUCHER SVP 
+NE PAS TOUCHER SVP 
+NE PAS TOUCHER SVP 
+NE PAS TOUCHER SVP 
+*/
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html lang="fr">
+        <Head >
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
