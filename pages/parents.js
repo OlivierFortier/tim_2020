3bd54e0e7bff3952276e96e70d3d355afe54feff
kimleegalarneau <kimlee.ny@hotmail.com>
2020-09-23 10:28:23 -0400

    nouvelle branche de page parents

---
+++ pages/parents.js
@@ -0,0 +1,14 @@
+export default function Parents() {
+    return (
+        <div style={{color: 'white'}}>
+            <h1>Toute une créature !</h1>
+            <a>Tous les parents sont uniques</a>
+            <ul>
+                <li>Tanesha</li>
+                <li>Vanessa</li>
+                <li>Brutus</li>
+                <li>Zeus</li>
+            </ul>
+        </div>
+    )
+}
