be0fbb9de860b58b5325ab54cf1120967b264b64
jijimy <jijimy123@gmail.com>
2020-09-23 10:35:38 -0400

    nouvelle branche etudiant

---
+++ pages/etudiants.js
@@ -0,0 +1,14 @@
+export default function Etudiants() {
+  return (
+    <div style={{ color: "white" }}>
+      <h1>voici la page pour les nouveaux étudiants</h1>
+      <ul>
+        <li>Étudiant 1</li>
+        <li>Étudiant 2</li>
+        <li>Étudiant 3</li>
+        <li>Étudiant 4</li>
+        <li>Étudiant 5</li>
+      </ul>
+    </div>
+  );
+}
