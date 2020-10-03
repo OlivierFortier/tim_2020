43e14e9bfc6fa347e7667e7190e55d81f4c498cf
Olivier Fortier <olivier_fortier@outlook.com>
2020-10-03 17:25:15 -0400

    extrait accueil dans son propre composant

---
+++ components/accueil.js
@@ -0,0 +1,90 @@
+export default function Accueil() {
+  return (
+    <>
+      <main>
+        <h1 className="titre-intro">
+          La<br></br> juxtaposition du <br></br>
+          <strong>logique</strong> et du<br></br>
+          <strong>créatif</strong> +
+        </h1>
+        <h2 id="nom-college">Collège de Maisonneuve</h2>
+      </main>
+      <aside>
+        <img
+          alt="image du theme"
+          src="https://images.unsplash.com/photo-1599666433232-2b222eb02b8c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+        ></img>
+        <div className="conteneur-bouton">
+          <button className="bouton-explorer" aria-label="explorer">
+            EXPLOREZ
+          </button>
+        </div>
+      </aside>
+
+      <style jsx>{`
+        main {
+          height: 70vh;
+          border-left: 3px solid white;
+          display: flex;
+          flex-direction: column;
+          justify-content: flex-end;
+        }
+
+        h1 {
+          a {
+            &:link,
+            &:visited,
+            &:hover,
+            &:active {
+              color: green;
+            }
+          }
+        }
+
+        .titre-intro {
+          color: #ffffff;
+          font-size: 4.5rem;
+          padding-left: 25px;
+        }
+
+        #nom-college {
+          color: #ffffff;
+          font-size: 30px;
+          padding-left: 25px;
+        }
+
+        aside {
+          height: 70vh;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: flex-end;
+
+          img {
+            z-index: 1;
+            width: auto;
+            height: 100%;
+          }
+        }
+
+        .conteneur-bouton {
+          position: relative;
+          height: 0 px;
+          width: 100%;
+          display: flex;
+          justify-content: flex-end;
+        }
+
+        .bouton-explorer {
+          position: absolute;
+          top: -2rem;
+          background-color: #fff;
+          border-radius: 50%;
+          height: 8rem;
+          width: 8rem;
+          z-index: 2;
+        }
+      `}</style>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,27 @@
 import Head from "next/head";
+import Accueil from "../components/accueil";
 
 export default function Home() {
   return (
     <div className="conteneur-page">
-
       <Head>
         <title>TIM | Accueil</title>
-        <meta name="Description" content="Page d'acceuil des Techniques d'Intégration Multimédia du collège Maisonneuve"></meta>
+        <meta
+          name="Description"
+          content="Page d'acceuil des Techniques d'Intégration Multimédia du collège Maisonneuve"
+        ></meta>
         <link rel="canonical" href="https://tim-2020.vercel.app/"></link>
       </Head>
 
-
-      <main>
-        <h1 className="titre-intro">
-          La<br></br> juxtaposition du <br></br>
-          <strong>logique</strong> et du<br></br>
-          <strong>créatif</strong> +
-        </h1>
-        <h2 id="nom-college">Collège de Maisonneuve</h2>
-      </main>
-      <aside>
-        <img alt="image du theme" src="https://images.unsplash.com/photo-1599666433232-2b222eb02b8c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"></img>
-        <div className="conteneur-bouton">
-          <button className="bouton-explorer" aria-label="explorer">EXPLOREZ</button>
-        </div>
-      </aside>
+      
+        <Accueil />
+      
 
       {/*il est préférable que vous utlisiez du css "local" et non "global"
       Cela fait que le css n'affecte rien d'autre que cette page, ce composant ou ce fichier, ce qui évite les erreurs.
       Cela se fait facilement à même le fichier js en utilisant la balise style jsx
       comme ci-dessous. .*/}
       <style jsx>{`
-        
         .conteneur-page {
           display: flex;
           flex-direction: row;
@@ -39,70 +29,6 @@ export default function Home() {
           align-items: center;
           width: 100%;
           max-width: 1500px;
-        }
-
-        main {
-          height: 70vh;
-          border-left: 3px solid white;
-          display: flex;
-          flex-direction: column;
-          justify-content: flex-end;
-        }
-
-        h1 {
-          a {
-            &:link,
-            &:visited,
-            &:hover,
-            &:active {
-              color: green;
-            }
-          }
-        }
-
-        .titre-intro {
-          color: #ffffff;
-          font-size: 4.5rem;
-          padding-left: 25px;
-        }
-
-        #nom-college {
-          color: #ffffff;
-          font-size: 30px;
-          padding-left: 25px;
-        }
-
-        aside {
-          height: 70vh;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: flex-end;
-
-          img {
-          z-index: 1;
-          width: auto;
-          height: 100%;
-          }
-
-        }
-
-        .conteneur-bouton {
-          position: relative;
-          height: 0 px;
-          width: 100%;
-          display: flex;
-          justify-content: flex-end;
-        }
-
-        .bouton-explorer {
-          position: absolute;
-          top: -2rem;
-          background-color: #fff;
-          border-radius: 50%;
-          height: 8rem;
-          width: 8rem;
-          z-index: 2;
         }
       `}</style>
     </div>
