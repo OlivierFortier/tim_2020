4a05fdb1914ecfcfc27e29f08a5db5c9d22b62ba
Olivier Fortier <olivier_fortier@outlook.com>
2020-10-08 15:11:35 -0400

    ajout react-tabs pour cours

--- pages/cours.js
+++ pages/cours.js
@@ -1,3 +1,5 @@
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+
 export default function Cours() {
   return (
     <div>
@@ -13,6 +15,23 @@ export default function Cours() {
           <option>la vidéo</option>
         </select>
       </h2>
+      <Tabs>
+        <TabList>
+          <Tab>Session 1</Tab>
+          <Tab>Session 2</Tab>
+          <Tab>Session 3</Tab>
+          <Tab>Session 4</Tab>
+          <Tab>Session 5</Tab>
+          <Tab>Session 6</Tab>
+        </TabList>
+
+        <TabPanel>
+          <h2>Any content 1</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 2</h2>
+        </TabPanel>
+      </Tabs>
     </div>
   );
 }
