bd8a4732a9997daefc464d460e99c0db5e6a98e7
Olivier Fortier <olivier_fortier@outlook.com>
2020-10-09 11:25:15 -0400

    début filtrer cours

--- pages/cours.js
+++ pages/cours.js
@@ -3,7 +3,9 @@ import NomCours from "../components/cours/nomCours";
 import { gql } from "graphql-request";
 import { faireRequeteGql, graphQLClient } from "../libs/requetesDonnes";
 import { resetIdCounter } from "react-tabs";
+import { useState } from "react";
 
+//fonction à appeler pour le tab, puisqu'on utilise un framework react isomorphe (rendu serveur & client)
 resetIdCounter();
 
 export default function Cours({ listeCours }) {
@@ -14,37 +16,39 @@ export default function Cours({ listeCours }) {
 
   //on trie les session qui ont des choix de cours
   const coursSession4 = listeCours.filter((cours) => cours.session === 4);
-  const coursSession4Tries = [...coursSession4].sort((a,b) => a.auChoix - b.auChoix);
+  const coursSession4Tries = [...coursSession4].sort( (a, b) => a.auChoix - b.auChoix );
   const coursSession5 = listeCours.filter((cours) => cours.session === 5);
-  const coursSession5Tries = [...coursSession5].sort((a,b) => a.auChoix - b.auChoix);
+  const coursSession5Tries = [...coursSession5].sort( (a, b) => a.auChoix - b.auChoix );
 
   const coursSession6 = listeCours.filter((cours) => cours.session === 6);
 
-  //regrouper tous les sessions dans 1 array
-  const tousLesCours = [
-    coursSession1,
-    coursSession2,
-    coursSession3,
-    coursSession4Tries,
-    coursSession5Tries,
-    coursSession6,
-  ];
+  const listeTousLesCours = [coursSession1, coursSession2, coursSession3, coursSession4Tries, coursSession5Tries, coursSession6]
 
-  //TODO : séparer les cours qui sont un choix de l'élève
+  //regrouper tous les sessions dans 1 array de l'état
+  const [tousLesCours, setTousLesCours] = useState(listeTousLesCours);
+
   //TODO : trier les cours selon le select
+
+  const [optionSelect, setOptionSelect] = useState("")
+
+  function filtrerCours(e) {
+    setOptionSelect(e.target.value)
+    const coursFiltres = tousLesCours.filter((cours)=> cours.type.map(type=> type === optionSelect ))
+    setTousLesCours(coursFiltres)
+  }
 
   return (
     <div>
       <h1>La liste des cours</h1>
       <h2>
         J'aime bien
-        <select>
-          <option>de tout</option>
-          <option>les jeux</option>
-          <option>le web</option>
-          <option>le design</option>
-          <option>la programmation</option>
-          <option>la vidéo</option>
+        <select value={optionSelect} onChange={(e)=> filtrerCours(e)}>
+          <option value="">de tout</option>
+          <option value="Jeux">les jeux</option>
+          <option value="Web">le web</option>
+          <option value="Design">le design</option>
+          <option value="Programmation">la programmation</option>
+          <option value="Vidéo et Audio">la vidéo</option>
         </select>
       </h2>
       <Tabs>
@@ -61,12 +65,11 @@ export default function Cours({ listeCours }) {
         {tousLesCours.map((session, indexSession) => {
           return (
             <TabPanel key={Math.random() * indexSession}>
-              { session.map((cours, indexCours) => {
-                
+              {session.map((coursFiltre, indexCours) => {
                 return (
                   <NomCours
                     key={Math.random() * indexCours}
-                    infoCours={cours}
+                    infoCours={coursFiltre}
                   />
                 );
               })}
