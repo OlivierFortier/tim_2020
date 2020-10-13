9b2b01fcf0690a0c40bf0e0e5b17de99b70676e3
Olivier Fortier <olivier_fortier@outlook.com>
2020-10-12 20:42:33 -0400

    organisation modules scss section cours

--- components/cours/detailsCours.js
+++ components/cours/detailsCours.js
@@ -1,6 +1,8 @@
+import styles from "./detailsCours.module.scss"
+
 export default function DetailsCours({infoCours, afficherCours}) {
     return (
-      <div>
+      <div className={styles.detailCours}>
         <button onClick={afficherCours}>
           retour aux autres cours
         </button>
