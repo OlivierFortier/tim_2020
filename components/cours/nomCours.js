3277d8af4b16b766f59ce4ec1f1d223ffa852986
jijimy <jijimy123@gmail.com>
2020-10-13 17:36:44 -0400

    Peaufiner les styles section cours

--- components/cours/nomCours.js
+++ components/cours/nomCours.js
@@ -1,12 +1,11 @@
-import styles from "./nomCours.module.scss"
+import styles from "./nomCours.module.scss";
 
 export default function NomCours({ infoCours, afficherCours }) {
-
   //TODO : gestion de l'icone selon le type du cours
 
   return (
     <div onClick={afficherCours} className={styles.cours}>
-      <h3>{infoCours.titre}</h3>
+      <h3 className={styles.titreCours}>{infoCours.titre}</h3>
       {infoCours.choixEntre && <i>choix entre {infoCours.choixEntre.titre}</i>}
       <span>
         <ul>
