c3e2e8c4a35b9b1a184135549fe13cb13ae2e07f
Olivier Fortier <olivier_fortier@outlook.com>
2020-10-16 16:35:41 -0400

    gestion icones dynamiques selon sujet cours

--- components/cours/nomCours.js
+++ components/cours/nomCours.js
@@ -1,22 +1,26 @@
 import styles from "./nomCours.module.scss";
-import {motion} from "framer-motion"
+import { motion } from "framer-motion";
+import { useIcone } from "../../hooks/useNomIcone";
 
 export default function NomCours({ infoCours, afficherCours }) {
-  //TODO : gestion de l'icone selon le type du cours
-
-  
+  //hook personnalisé pour afficher une icone selont le sujet du cours
+  const Icone = useIcone(infoCours.sujetPrincipal[0]);
 
   return (
-    <motion.div layout initial={{x: -500, opacity:0}} animate={{x:0, opacity:1}} exit={{x:500, opacity:0}} onClick={afficherCours} className={styles.cours}>
-      <motion.h3 layout className={styles.titreCours}>{infoCours.titre}</motion.h3>
+    <motion.div
+      layout
+      initial={{ x: -500, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      exit={{ x: 500, opacity: 0 }}
+      onClick={afficherCours}
+      className={styles.cours}
+    >
+      <motion.h3 layout className={styles.titreCours}>
+        {infoCours.titre}
+      </motion.h3>
       {infoCours.choixEntre && <i>choix entre {infoCours.choixEntre.titre}</i>}
       <motion.span layout>
-        <ul className={styles.listeTypes}>
-          {infoCours.typesSecondaires &&
-            infoCours.typesSecondaires.map((type, index) => {
-              return <li key={Math.random() * index}>{type}</li>;
-            })}
-        </ul>
+        <Icone fontSize="30"></Icone>
       </motion.span>
     </motion.div>
   );
