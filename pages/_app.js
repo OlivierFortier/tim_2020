283aab97f9ea7c8bc29e8f9b45115a36c8dfa09c
Olivier Fortier <olivier_fortier@outlook.com>
2020-10-21 15:38:16 -0400

    base carousel pour grille prof

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import "normalize.css";
 import "../styles/styles.scss";
 import "react-tabs/style/react-tabs.css";
+import 'swiper/swiper.scss';
+import 'swiper/components/pagination/pagination.scss';
 import Layout from "../components/layout";
 import { FournisseurTheme } from "../hooks/contexteTheme";
 import { AnimatePresence, AnimateSharedLayout } from "framer-motion";
