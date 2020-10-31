5cd1b523f67336b8e74d2dbc8010f0d7b6a3ae83
Olivier Fortier <olivier_fortier@outlook.com>
2020-10-30 22:34:44 -0400

    distribuer en composant la grille prof

--- pages/professeurs/grille/index.js
+++ pages/professeurs/grille/index.js
@@ -1,13 +1,11 @@
 import { motion } from "framer-motion";
 import styles from "./grille.module.scss";
-import SwiperCore, { Pagination, Virtual } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
 import { gql } from "graphql-request";
 import { faireRequeteGql } from "../../../libs/requetesDonnes";
-import CarteProf from "../../../components/profs/CarteProf";
 import Head from "next/head";
+import SwiperProfs from "../../../components/profs/SwiperProfs";
 
-SwiperCore.use([Pagination]);
+
 
 export default function Grille({ listeProfs }) {
   return (
@@ -20,28 +18,7 @@ export default function Grille({ listeProfs }) {
         ></meta>
         <link rel="canonical" href="https://tim-2020.vercel.app/professeurs/grille"></link>
       </Head>
-      <Swiper
-        pagination={{
-          clickable: true,
-          el: "#lesPoints",
-        }}
-        spaceBetween={0}
-        slidesPerView={4}
-        slidesPerColumn={3}
-        slidesPerColumnFill="row"
-        observer="true"
-        style={{ paddingTop: "2%", paddingLeft: "5%" }}
-      >
-        {listeProfs.map((infoProf) => {
-          return (
-            <SwiperSlide key={infoProf.nom}>
-              <CarteProf prof={infoProf} />
-            </SwiperSlide>
-          );
-        })}
-
-        <div id="lesPoints" className={styles.points}></div>
-      </Swiper>
+     <SwiperProfs listeProfs={listeProfs}/>
     </motion.div>
   );
 }
