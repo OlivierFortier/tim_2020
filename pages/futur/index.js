4150aa4f6d6be517b0b8ad7a9561ec1b2cf68d93
Olivier Fortier <olivier_fortier@outlook.com>
2020-10-31 16:06:24 -0400

    layout de base page futur

---
+++ pages/futur/index.js
@@ -0,0 +1,24 @@
+import styles from "./futur.module.scss";
+
+export default function Futur() {
+  return (
+    <main className={styles.conteneurPage}>
+
+      <section className={styles.sectionUni}>
+        <h1>Perspective universitaire</h1>
+        <h3>Poursuivre ses études et approfondir ses connaissances.</h3>
+      </section>
+
+      <section className={styles.sectionStage}>
+        <h1>Stages</h1>
+        <h3>Mettre en pratique ses apprentissages.</h3>
+      </section>
+
+      <section className={styles.sectionEmploi}>
+        <h1>Perspectives d'emploi</h1>
+        <h3>Devenir un professionnel dans le domaine.</h3>
+      </section>
+
+    </main>
+  );
+}
