758ff30af583a760ab672a1b07a9d3c9aeb7e5b3
Olivier Fortier <olivier_fortier@outlook.com>
2020-11-06 18:35:54 -0500

    ajout route grille vers prof

--- components/profs/CarteProf.js
+++ components/profs/CarteProf.js
@@ -1,30 +1,33 @@
 import styles from "./CarteProf.module.scss";
 import Image from "next/image";
+import Link from "next/link"
 
 export default function CarteProf({ prof }) {
   return (
-    <div className={styles.unProf}>
-      {prof?.photo?.url ? (
-        <Image
-          width={266}
-          height={254}
-          src={prof?.photo?.url}
-          loading="lazy"
-          className={styles.imgProf}
-          alt={`photo de ${prof.nom}`}
-        />
-      ) : (
-        <Image
-          width={266}
-          height={254}
-          src="/images/cam.jpg"
-          loading="lazy"
-          className={styles.imgProf}
-          alt=""
-        />
-      )}
-      <h3>{prof.nom}</h3>
-      <h4>{prof.specialisation}</h4>
-    </div>
+    <Link href="/professeurs/unprof">
+      <div className={styles.unProf} >
+        {prof?.photo?.url ? (
+          <Image
+            width={266}
+            height={254}
+            src={prof?.photo?.url}
+            loading="lazy"
+            className={styles.imgProf}
+            alt={`photo de ${prof.nom}`}
+          />
+        ) : (
+          <Image
+            width={266}
+            height={254}
+            src="/images/cam.jpg"
+            loading="lazy"
+            className={styles.imgProf}
+            alt=""
+          />
+        )}
+        <h3>{prof.nom}</h3>
+        <h4>{prof.specialisation}</h4>
+      </div>
+    </Link>
   );
 }
