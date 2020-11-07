e7f7552d74d72a47db98b4506d74221a5b9edf1a
Olivier Fortier <olivier_fortier@outlook.com>
2020-11-06 20:16:08 -0500

    page prof dynamique

---
+++ pages/professeurs/[slug].js
@@ -0,0 +1,87 @@
+import styles from "./unprof.module.scss";
+import Image from "next/image";
+import { gql } from "graphql-request";
+import { faireRequeteGql } from "../../libs/requetesDonnes";
+import Markdown from "markdown-to-jsx";
+
+export default function PageUnProfesseur(leProf) {
+  return (
+    <main className={styles.conteneurPage}>
+      <section>
+        <Image
+          className={styles.imgProf}
+          src={leProf.photo.url}
+          width={453}
+          height={620}
+        />
+        <article>
+          <span className={styles.conteneurTitres}>
+            <h1>Denis Pellerin</h1>
+            <h5>Professeur de design et de vidéo</h5>
+          </span>
+
+          {/* markdown plus tard */}
+          <div className={styles.contenuTexte}>
+            <Markdown>
+              {leProf.description}
+            </Markdown>
+          
+          </div>
+        </article>
+      </section>
+    </main>
+  );
+}
+
+export async function getStaticProps({ params }) {
+  const requeteGql = gql`
+    query Professeurs($slug: String!) {
+      professeurCollection(where: { slug: $slug }) {
+        items {
+          nom
+          description
+          slug
+          photo {
+            url
+          }
+        }
+      }
+    }
+  `;
+
+  const lesVariables = { slug: params.slug };
+
+  const json = await faireRequeteGql(requeteGql, lesVariables);
+
+  const leProf = json.professeurCollection.items[0];
+
+  console.log(leProf);
+
+  return { props: leProf, revalidate: 1 };
+}
+
+export async function getStaticPaths() {
+  const requeteGqlSlug = gql`
+    {
+      professeurCollection {
+        items {
+          slug
+        }
+      }
+    }
+  `;
+
+  const donnees = await faireRequeteGql(requeteGqlSlug);
+
+  const listeProfs = donnees.professeurCollection.items;
+
+  const slugs = listeProfs.map((prof) => prof.slug);
+
+  const paths = slugs.map((slug) => ({ params: { slug } }));
+
+  return {
+    paths,
+
+    fallback: false,
+  };
+}
