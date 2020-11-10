a48a09fd9589ee12a34cf376b00e2af8428a8012
Olivier Fortier <olivier_fortier@outlook.com>
2020-11-10 14:26:08 -0500

    chargement mobile

--- components/header/enTete.js
+++ components/header/enTete.js
@@ -2,12 +2,17 @@ import Menu from "./menu";
 import BoutonSelectionTheme from "./boutonSelectionTheme";
 import styles from "./enTete.module.scss";
 
+
 export default function EnTete() {
 
 
+
+  
+  // if on est sur composant page intro
+
   return (
     <>
-      <header className={styles.enTete}>
+      <header id="header-site" className={styles.enTete}>
         <span>
           <BoutonSelectionTheme />
           <h1 className={styles.titre}>
