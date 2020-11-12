c23bb094161cb86f67d918b603decf29bcedd1e2
Olivier Fortier <olivier_fortier@outlook.com>
2020-11-11 23:16:58 -0500

    début mobile page prof

--- pages/professeurs/index.js
+++ pages/professeurs/index.js
@@ -36,111 +36,191 @@ export default function Professeurs() {
         <span>
           <Link href="/professeurs/grille">
             <motion.div
-            className={styles.conteneurImg}
+              className={styles.conteneurImg}
               initial={{
                 opacity: 0,
               }}
               animate={{ opacity: 1, x: "0%", y: "0%", scale: 0.8 }}
             >
-              <Image width={120} height={120} src="/images/cam.jpg" />
+              <Image
+              className={styles.imgProf}
+                // width={120}
+                // height={120}
+                loading="eager"
+                layout="fill"
+                unsized="true"
+                src="/images/cam.jpg"
+              />
             </motion.div>
           </Link>
           <Link href="/professeurs/grille">
             <motion.div
-            className={styles.conteneurImg}
+              className={styles.conteneurImg}
               initial={{
                 opacity: 0,
               }}
               animate={{ opacity: 1, x: "100%", y: "-10%", scale: 1.4 }}
             >
-              <Image width={120} height={120} src="/images/cam.jpg" alt={`photo de ${"prof.nom"}`} />
+              <Image
+              className={styles.imgProf}
+                // width={120}
+                // height={120}
+                loading="eager"
+                layout="fill"
+                unsized="true"
+                src="/images/cam.jpg"
+                alt={`photo de ${"prof.nom"}`}
+              />
             </motion.div>
           </Link>
           <Link href="/professeurs/grille">
             <motion.div
-            className={styles.conteneurImg}
+              className={styles.conteneurImg}
               initial={{
                 opacity: 0,
               }}
               animate={{ opacity: 1, x: "-10%", y: "140%" }}
             >
-              <Image width={120} height={120} src="/images/cam.jpg" alt={`photo de ${"prof.nom"}`} />
+              <Image
+              className={styles.imgProf}
+                // width={120}
+                // height={120}
+                loading="eager"
+                layout="fill"
+                unsized="true"
+                src="/images/cam.jpg"
+                alt={`photo de ${"prof.nom"}`}
+              />
             </motion.div>
           </Link>
         </span>
         <span>
           <Link href="/professeurs/grille">
             <motion.div
-            className={styles.conteneurImg}
+              className={styles.conteneurImg}
               initial={{
                 opacity: 0,
               }}
               animate={{ opacity: 1, scale: 3.6, y: "70%" }}
             >
-              <Image width={120} height={120} src="/images/cam.jpg" alt={`photo de ${"prof.nom"}`} />
+              <Image
+              className={styles.imgProf}
+                // width={120}
+                // height={120}
+                loading="eager"
+                layout="fill"
+                unsized="true"
+                src="/images/cam.jpg"
+                alt={`photo de ${"prof.nom"}`}
+              />
             </motion.div>
           </Link>
         </span>
         <span>
           <motion.div
-          className={styles.conteneurImg}
+            className={styles.conteneurImg}
             animate={{ x: "-40%", y: "-20%" }}
             className={styles.cercle}
           ></motion.div>
           <Link href="/professeurs/grille">
             <motion.div
-            className={styles.conteneurImg}
+              className={styles.conteneurImg}
               initial={{
                 opacity: 0,
               }}
               animate={{ opacity: 1, x: "20%", y: "-90%", scale: 0.9 }}
             >
-              <Image width={120} height={120} src="/images/cam.jpg" alt={`photo de ${"prof.nom"}`} />
+              <Image
+              className={styles.imgProf}
+                // width={120}
+                // height={120}
+                loading="eager"
+                layout="fill"
+                unsized="true"
+                src="/images/cam.jpg"
+                alt={`photo de ${"prof.nom"}`}
+              />
             </motion.div>
           </Link>
           <Link href="/professeurs/grille">
             <motion.div
-            className={styles.conteneurImg}
+              className={styles.conteneurImg}
               initial={{
                 opacity: 0,
               }}
               animate={{ opacity: 1, x: "145%", y: "-140%" }}
             >
-              <Image width={120} height={120} src="/images/cam.jpg" alt={`photo de ${"prof.nom"}`} />
+              <Image
+              className={styles.imgProf}
+                // width={120}
+                // height={120}
+                loading="eager"
+                layout="fill"
+                unsized="true"
+                src="/images/cam.jpg"
+                alt={`photo de ${"prof.nom"}`}
+              />
             </motion.div>
           </Link>
           <Link href="/professeurs/grille">
             <motion.div
-            className={styles.conteneurImg}
+              className={styles.conteneurImg}
               style={{ zIndex: -1 }}
               initial={{
                 opacity: 0,
               }}
               animate={{ opacity: 1, x: "10%", y: "40%" }}
             >
-              <Image width={120} height={120} src="/images/cam.jpg" alt={`photo de ${"prof.nom"}`} />
+              <Image
+              className={styles.imgProf}
+                // width={120}
+                // height={120}
+                loading="eager"
+                layout="fill"
+                unsized="true"
+                src="/images/cam.jpg"
+                alt={`photo de ${"prof.nom"}`}
+              />
             </motion.div>
           </Link>
           <Link href="/professeurs/grille">
             <motion.div
-            className={styles.conteneurImg}
+              className={styles.conteneurImg}
               initial={{
                 opacity: 0,
               }}
               animate={{ opacity: 1, x: "-260%", y: "150%", scale: 1.4 }}
             >
-              <Image width={120} height={120} src="/images/cam.jpg" alt={`photo de ${"prof.nom"}`} />
+              <Image
+              className={styles.imgProf}
+                // width={120}
+                // height={120}
+                loading="eager"
+                layout="fill"
+                unsized="true"
+                src="/images/cam.jpg"
+                alt={`photo de ${"prof.nom"}`}
+              />
             </motion.div>
           </Link>
           <Link href="/professeurs/grille">
             <motion.div
-            className={styles.conteneurImg}
+              className={styles.conteneurImg}
               initial={{
                 opacity: 0,
               }}
               animate={{ opacity: 1, x: "-300%", y: "340%", scale: 2 }}
             >
-              <Image width={120} height={120} src="/images/cam.jpg" alt={`photo de ${"prof.nom"}`} />
+              <Image
+              className={styles.imgProf}
+                // width={120}
+                // height={120}
+                loading="eager"
+                layout="fill"
+                unsized="true"
+                src="/images/cam.jpg"
+                alt={`photo de ${"prof.nom"}`}
+              />
             </motion.div>
           </Link>
         </span>
