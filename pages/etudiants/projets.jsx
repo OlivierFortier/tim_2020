b3e38745bfcdb72ac5fb31d3221e63eb7f525198
Olivier Fortier <olivier_fortier@outlook.com>
2020-11-14 15:48:51 -0500

    grille projets fonctionnelle

---
+++ pages/etudiants/projets.jsx
@@ -0,0 +1,68 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Pagination } from "swiper";
+import styles from "./gallerie.module.scss";
+import CarteProjet from "../../components/etudiants/CarteProjet";
+import { gql } from "graphql-request";
+import { faireRequeteGql } from "../../libs/requetesDonnes";
+import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+
+SwiperCore.use([Pagination]);
+
+export default function projets(lesProjets) {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      className={styles.conteneurPage}
+    >
+      <h1>Projets étudiants</h1>
+      <main className={styles.conteneurSwiper}>
+        <Swiper
+          pagination={{
+            clickable: true,
+            el: "#lesPoints",
+          }}
+          spaceBetween={30}
+          slidesPerView={3}
+          slidesPerColumn={2}
+          slidesPerColumnFill="row"
+        >
+          {lesProjets.items.map((projet, index) => {
+            return (
+              <SwiperSlide className={styles.uneSlide} key={index}>
+                <CarteProjet infoCarte={projet} />
+              </SwiperSlide>
+            );
+          })}
+
+          <div id="lesPoints" className={styles.points}></div>
+        </Swiper>
+      </main>
+    </motion.div>
+  );
+}
+
+export async function getStaticProps({ params }) {
+  const requeteGql = gql`
+    query {
+      projetTudiantCollection {
+        items {
+          titreDuProjet
+          typeDuProjet
+          lienExterne
+          captureDcran {
+            url
+          }
+        }
+      }
+    }
+  `;
+
+  const json = await faireRequeteGql(requeteGql);
+
+  const lesProjets = json.projetTudiantCollection;
+
+  return { props: lesProjets, revalidate: 1 };
+}
