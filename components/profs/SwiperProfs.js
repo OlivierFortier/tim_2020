bcb54e7f7b27dec5bdaeada35f1719495ad43859
Olivier Fortier <olivier_fortier@outlook.com>
2020-11-20 21:26:43 -0500

    correction grille profs

--- components/profs/SwiperProfs.js
+++ components/profs/SwiperProfs.js
@@ -9,27 +9,29 @@ SwiperCore.use([Pagination]);
 
 export default function SwiperProfs({ listeProfs }) {
   return (
-    <Swiper
-      pagination={{
-        clickable: true,
-        el: "#lesPoints",
-      }}
-      spaceBetween={0}
-      slidesPerView={4}
-      slidesPerColumn={3}
-      slidesPerColumnFill="row"
-      observer={true}
-      style={{ paddingTop: "2%", paddingLeft: "5%" }}
-    >
-      {listeProfs.map((infoProf) => {
-        return (
-          <SwiperSlide key={infoProf.nom}>
-            <CarteProf prof={infoProf} />
-          </SwiperSlide>
-        );
-      })}
-
-      <div id="lesPoints" className={styles.points}></div>
-    </Swiper>
+    <main>
+      <Swiper
+        pagination={{
+          clickable: true,
+          el: "#lesPoints",
+        }}
+        spaceBetween={0}
+        slidesPerView={4}
+        slidesPerColumn={3}
+        slidesPerColumnFill="row"
+        observer
+        // style={{ paddingTop: "2%", paddingLeft: "5%" }}
+      >
+        {listeProfs.map((infoProf) => {
+          return (
+            <SwiperSlide className={styles.uneSlide} key={infoProf.nom}>
+              <CarteProf prof={infoProf} />
+            </SwiperSlide>
+          );
+        })}
+  
+        <div id="lesPoints" className={styles.points}></div>
+      </Swiper>
+    </main>
   );
 }
