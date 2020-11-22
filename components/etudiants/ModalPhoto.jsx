cc166728834daf899cd1581bef352697e442b120
Olivier Fortier <olivier_fortier@outlook.com>
2020-11-22 12:07:46 -0500

    fonctionnement du lightbox

--- components/etudiants/ModalPhoto.jsx
+++ components/etudiants/ModalPhoto.jsx
@@ -8,20 +8,30 @@ export default function ModalPhoto({ infoPhoto, ouvert, fermerModal }) {
       style={{
         overlay: {
           zIndex: 10,
-        }
+        },
       }}
       overlayClassName={styles.leModalFond}
       className={styles.leModal}
       isOpen={ouvert}
       onRequestClose={fermerModal}
     >
-      <div className={styles.conteneurModal}>
+      <div
+        onClick={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          if (e.target === e.currentTarget) {
+            fermerModal();
+          }
+        }}
+        className={styles.conteneurModal}
+      >
         <div className={styles.conteneurImage}>
           {infoPhoto && ouvert && (
             <Image
               src={infoPhoto.photo.url}
               layout="fill"
               unsized
+              quality={75}
               className={styles.image}
             />
           )}
