1029969b0c0659eccc0680273de6717ce3ab039d
Olivier Fortier <olivier_fortier@outlook.com>
2020-11-22 19:29:31 -0500

    barre de navigation secondaire présente

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -3,8 +3,15 @@ import styles from "./Navigation.module.scss"
 export default function Navigation() {
   return (
     <footer className={styles.conteneurNavigation}>
-      <span>
-        <progress value={1} max={8}></progress>
+      <span className={styles.barreNavigation}>
+        <button className={styles.boutonUneSection}>01</button>
+        <button className={styles.boutonUneSection}>02</button>
+        <button className={styles.boutonUneSection}>03</button>
+        <button className={styles.boutonUneSection}>04</button>
+        <button className={styles.boutonUneSection}>05</button>
+        <button className={styles.boutonUneSection}>06</button>
+        <button className={styles.boutonUneSection}>07</button>
+        <progress value={6} max={7} className={styles.ligneProgres}></progress>
       </span>
     </footer>
   );
