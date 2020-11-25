a4eb558d4301b3dcc8187af81ad07576576402bd
Olivier Fortier <olivier_fortier@outlook.com>
2020-11-24 22:16:13 -0500

    couleur selon theme intro

--- pages/introduction.js
+++ pages/introduction.js
@@ -2,65 +2,111 @@ import { motion } from "framer-motion";
 import { Canvas } from "react-three-fiber";
 import CubeTransparent from "../components/3d/CubeTransparent";
 import styles from "./introduction.module.scss";
-import Link from "next/link"
+import Link from "next/link";
 import Head from "next/head";
-import {useEffect, useState} from "react"
+import { useEffect, useState } from "react";
 import { useListeThemes, useTheme } from "../hooks/contexteTheme";
 
 export default function Introduction() {
-
   // changer couleur selon theme
   const theme = useTheme();
   const listeThemes = useListeThemes();
-  const [lesStyles, setLesStyles] = useState({ couleurCube: "#f3f1f1" });
+  const [lesStyles, setLesStyles] = useState({
+    couleurCube: "#f3f1f1",
+    couleurTexte: "#f3f1f1",
+    couleurBouton: "#f3f1f1",
+    couleurTexteBouton: "black",
+  });
 
   useEffect(() => {
     switch (theme) {
       case listeThemes.art:
-        setLesStyles({ couleurCube: "#f3f1f1" });
+        setLesStyles({
+          couleurCube: "#f3f1f1",
+          couleurTexte: "#f3f1f1",
+          couleurBouton: "#f3f1f1",
+          couleurTexteBouton: "black",
+        });
         break;
 
       case listeThemes.code:
-        setLesStyles({ couleurCube: "#f3f1f1" });
+        setLesStyles({
+          couleurCube: "#f3f1f1",
+          couleurTexte: "#f3f1f1",
+          couleurBouton: "#f3f1f1",
+          couleurTexteBouton: "black",
+        });
         break;
 
       case listeThemes.parent:
-        setLesStyles({ couleurCube: "black" });
+        setLesStyles({
+          couleurCube: "black",
+          couleurTexte: "black",
+          couleurBouton: "black",
+          couleurTexteBouton: "#f3f1f1",
+        });
         break;
 
       default:
-        setLesStyles({ couleurCube: "#f3f1f1" });
+        setLesStyles({
+          couleurCube: "#f3f1f1",
+          couleurTexte: "#f3f1f1",
+          couleurBouton: "#f3f1f1",
+          couleurTexteBouton: "black",
+        });
         break;
     }
-  },[theme]);
+  }, [theme]);
 
   return (
     <motion.div
-      initial={{ x: 5, opacity: 0, transition:{ ease: "easeInOut", duration: 0.5}}}
-      animate={{ x: 0, opacity: 1}}
-      exit={{ x: -40, opacity: 0,  transition: {ease: "linear", duration: 0.2}}}
+      initial={{
+        x: 5,
+        opacity: 0,
+        transition: { ease: "easeInOut", duration: 0.5 },
+      }}
+      animate={{ x: 0, opacity: 1 }}
+      exit={{
+        x: -40,
+        opacity: 0,
+        transition: { ease: "linear", duration: 0.2 },
+      }}
       className={styles.conteneur}
     >
-     <Head>
+      <Head>
         <title>TIM | Introduction</title>
         <meta charset="UTF-8"></meta>
-        <meta name="Description"
-              content="Page d'introduction de la technique d'Intégration Multimédia du collège Maisonneuve"
+        <meta
+          name="Description"
+          content="Page d'introduction de la technique d'Intégration Multimédia du collège Maisonneuve"
         ></meta>
-        <link rel="canonical" href="https://tim-2020.vercel.app/introduction"></link>
+        <link
+          rel="canonical"
+          href="https://tim-2020.vercel.app/introduction"
+        ></link>
       </Head>
 
       <h1 className={styles.headerPage}>
         Envie d'étudier le <b>multimédia ?</b>
       </h1>
-      <motion.div 
-      initial={{ opacity: 0, x: 5, y: -5}}
-      animate={{ opacity: 1, x: 0, y: 0, transition:{ duration: 2, ease: "linear"}}}
-      className={styles.conteneurCube}>
+      <motion.div
+        initial={{ opacity: 0, x: 5, y: -5 }}
+        animate={{
+          opacity: 1,
+          x: 0,
+          y: 0,
+          transition: { duration: 2, ease: "linear" },
+        }}
+        className={styles.conteneurCube}
+      >
         <Canvas className={styles.canvas3D}>
           <ambientLight />
           <pointLight position={[10, 10, 10]} />
-          <CubeTransparent couleur3D={lesStyles.couleurCube} position={[0, 0, 0]} scale={[3, 3, 3]} />
+          <CubeTransparent
+            couleur3D={lesStyles.couleurCube}
+            position={[0, 0, 0]}
+            scale={[3, 3, 3]}
+          />
         </Canvas>
       </motion.div>
 
@@ -72,13 +118,14 @@ export default function Introduction() {
       </p>
 
       <Link href="/cours">
-       <a className={styles.lienCours}>LISTE DES COURS</a>
+        <a style={{backgroundColor: lesStyles.couleurTexte}} className={styles.lienCours}>LISTE DES COURS</a>
       </Link>
 
       <Link href="/cours">
         <motion.button
           whileHover={{ scale: 1.1 }}
           className={styles.boutonCours}
+          style={{backgroundColor: lesStyles.couleurBouton, color: lesStyles.couleurTexteBouton}}
         >
           LISTE DES COURS
         </motion.button>
