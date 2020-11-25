1bd72c0ab4547a6f4db1250b634d2120647846b1
Olivier Fortier <olivier_fortier@outlook.com>
2020-11-24 23:41:01 -0500

    ajustement bug nav secondaire

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -8,32 +8,30 @@ import { etatMenu } from "./etat_global/EtatMenu";
 import { useListeThemes, useTheme } from "../hooks/contexteTheme";
 
 export default function Navigation() {
+  // gestion du theme
+  const theme = useTheme();
+  const listeThemes = useListeThemes();
+  const [lesStyles, setLesStyles] = useState({ couleurNav: "#f3f1f1" });
 
-    // gestion du theme
-    const theme = useTheme()
-    const listeThemes = useListeThemes()
-    const [lesStyles, setLesStyles] = useState({couleurNav: "#f3f1f1"})
+  useEffect(() => {
+    switch (theme) {
+      case listeThemes.art:
+        setLesStyles({ couleurNav: "#f3f1f1" });
+        break;
 
-    useEffect(()=>{
-      switch (theme) {
-        case listeThemes.art:
-         setLesStyles({couleurNav: "#f3f1f1"})
-          break;
-  
-        case listeThemes.code:
-          setLesStyles({couleurNav: "#f3f1f1"})
-          break;
-  
-        case listeThemes.parent:
-          setLesStyles({couleurNav: "black"})
-          break;
-  
-        default:
-          setLesStyles({couleurNav: "#f3f1f1"})
-          break;
-      }
-    },[theme])
+      case listeThemes.code:
+        setLesStyles({ couleurNav: "#f3f1f1" });
+        break;
 
+      case listeThemes.parent:
+        setLesStyles({ couleurNav: "black" });
+        break;
+
+      default:
+        setLesStyles({ couleurNav: "#f3f1f1" });
+        break;
+    }
+  }, [theme]);
 
   // obtenir l'état du menu pour faire apparaitre ou disparaitre la nav secondaire
   const snapShot = useProxy(etatMenu);
@@ -50,30 +48,30 @@ export default function Navigation() {
 
   // ajuster le progres de la barre selon la page ou nous sommes + ajuster marge de la barre
   useEffect(() => {
-    setMargeDessus((ancienneMarge) => "");
-    if (router.pathname === "/") {
-      setProgresBarre((ancienProgres) => "0%");
-      if (ecranMobile) setMargeDessus((ancienneMarge) => "-10%");
-      if (ecranTablette) setMargeDessus((ancienneMarge) => "-10%");
+    setMargeDessus("");
+    if (router.pathname === "") {
+      setProgresBarre("0%");
+      if (ecranMobile) setMargeDessus("-10%");
+      if (ecranTablette) setMargeDessus("-10%");
     }
-    if (router.pathname === "/introduction")
-      setMargeDessus((ancienneMarge) => "-25% !important");
-    setProgresBarre((ancienProgres) => "10%");
-    if (router.pathname === "/cours") setProgresBarre((ancienProgres) => "30%");
-    if (router.pathname.includes("/professeurs"))
-      setProgresBarre((ancienProgres) => "50%");
-    setMargeDessus((ancienneMarge) => "-2%");
+    if (router.pathname === "/introduction") {
+      setMargeDessus("-25% !important");
+      setProgresBarre("10%");
+    }
+    if (router.pathname === "/cours") setProgresBarre("30%");
+    if (router.pathname.includes("/professeurs")) {
+      setProgresBarre("50%");
+      setMargeDessus("-2%");
+    }
     if (router.pathname.includes("/etudiants")) {
-      setProgresBarre((ancienProgres) => "70%");
-      setMargeDessus((ancienneMarge) => "2%");
-      if (ecranMobile) setMargeDessus((ancienneMarge) => "-10%");
-      if (ecranTablette) setMargeDessus((ancienneMarge) => "-10%");
+      setProgresBarre("70%");
+      setMargeDessus("2%");
+      if (ecranMobile) setMargeDessus("-10%");
+      if (ecranTablette) setMargeDessus("-10%");
     }
-    if (router.pathname.includes("/futur"))
-      setProgresBarre((ancienProgres) => "90%");
-    if (router.pathname === "/inscription")
-      setProgresBarre((ancienProgres) => "100%");
-  }, [router.pathname]);
+    if (router.pathname.includes("/futur")) setProgresBarre("90%");
+    if (router.pathname === "/inscription") setProgresBarre("100%");
+  }, [router]);
 
   return (
     !snapShot.menuEstOuvert && (
@@ -84,25 +82,60 @@ export default function Navigation() {
       >
         <span className={styles.barreNavigation}>
           <Link href="/">
-            <button style={{color: lesStyles.couleurNav}} className={styles.boutonUneSection}>01</button>
+            <button
+              style={{ color: lesStyles.couleurNav }}
+              className={styles.boutonUneSection}
+            >
+              01
+            </button>
           </Link>
           <Link href="/introduction">
-            <button style={{color: lesStyles.couleurNav}} className={styles.boutonUneSection}>02</button>
+            <button
+              style={{ color: lesStyles.couleurNav }}
+              className={styles.boutonUneSection}
+            >
+              02
+            </button>
           </Link>
           <Link href="/cours">
-            <button style={{color: lesStyles.couleurNav}} className={styles.boutonUneSection}>03</button>
+            <button
+              style={{ color: lesStyles.couleurNav }}
+              className={styles.boutonUneSection}
+            >
+              03
+            </button>
           </Link>
           <Link href="/professeurs">
-            <button style={{color: lesStyles.couleurNav}} className={styles.boutonUneSection}>04</button>
+            <button
+              style={{ color: lesStyles.couleurNav }}
+              className={styles.boutonUneSection}
+            >
+              04
+            </button>
           </Link>
           <Link href="/etudiants">
-            <button style={{color: lesStyles.couleurNav}} className={styles.boutonUneSection}>05</button>
+            <button
+              style={{ color: lesStyles.couleurNav }}
+              className={styles.boutonUneSection}
+            >
+              05
+            </button>
           </Link>
           <Link href="/futur">
-            <button style={{color: lesStyles.couleurNav}} className={styles.boutonUneSection}>06</button>
+            <button
+              style={{ color: lesStyles.couleurNav }}
+              className={styles.boutonUneSection}
+            >
+              06
+            </button>
           </Link>
           <Link href="/inscription">
-            <button style={{color: lesStyles.couleurNav}} className={styles.boutonUneSection}>07</button>
+            <button
+              style={{ color: lesStyles.couleurNav }}
+              className={styles.boutonUneSection}
+            >
+              07
+            </button>
           </Link>
           <span
             style={{ width: progresBarre, borderColor: lesStyles.couleurNav }}
