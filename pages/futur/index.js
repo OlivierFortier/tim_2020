19c539636fcdf4c30f7af81e51ff7c60d8602772
Olivier Fortier <olivier_fortier@outlook.com>
2020-12-02 18:50:59 -0500

    couleurs hover selon theme page futur

--- pages/futur/index.js
+++ pages/futur/index.js
@@ -1,16 +1,57 @@
 import styles from "./futur.module.scss";
 import { motion } from "framer-motion";
 import Head from "next/head";
-
+import { useTheme, useListeThemes } from "../../hooks/contexteTheme";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Futur() {
+  const theme = useTheme();
+  const listeTheme = useListeThemes();
+
+  const [lesStyles, setLesStyles] = useState({
+    couleurHover: "#F16242",
+    couleurInitiale : "#f3f1f1"
+  });
+
+  useEffect(() => {
+    switch (theme) {
+      case listeTheme.art:
+        setLesStyles({
+          couleurHover: "#F16242",
+          couleurInitiale : "#f3f1f1"
+        });
+        break;
+
+      case listeTheme.code:
+        setLesStyles({
+          couleurHover: "#24DC48",
+          couleurInitiale : "#f3f1f1"
+        });
+        break;
+
+      case listeTheme.parent:
+        setLesStyles({
+          couleurHover: "#4363CA",
+          couleurInitiale : "#000000"
+        });
+        break;
+
+      default:
+        setLesStyles({
+          couleurHover: "#F16242",
+          couleurInitiale : "#f3f1f1"
+        });
+        break;
+    }
+  }, [theme]);
+
   return (
     <motion.main
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      transition={{duration: 0.75}}
+      transition={{ duration: 0.75 }}
       key="lesFuturs"
       className={styles.conteneurPage}
     >
@@ -21,10 +62,7 @@ export default function Futur() {
           content="Page qui décrit les opportunités après le DEC: Les stages, les perspectives d'emplois et les universités, aux Techniques d'Intégration Multimédia du Collège Maisonneuve"
         ></meta>
         <link rel="canonical" href="https://tim-2020.vercel.app/futur"></link>
-        <meta
-          property="og:title"
-          content="Futur | TIM Maisonneuve"
-        />
+        <meta property="og:title" content="Futur | TIM Maisonneuve" />
         <meta property="og:url" content="https://tim-2020.vercel.app/futur" />
         <meta
           property="og:description"
@@ -36,21 +74,39 @@ export default function Futur() {
       </div>
 
       <Link href="/futur/perspectives-universitaires">
-        <motion.section whileHover={{ color: "#4F638D"}} whileTap={{scale: 0.9}} className={styles.sectionUni}>
+        <motion.section
+        initial={{color : lesStyles.couleurInitiale}}
+        animate={{color : lesStyles.couleurInitiale}}
+          whileHover={{ color: lesStyles.couleurHover }}
+          whileTap={{ scale: 0.9 }}
+          className={styles.sectionUni}
+        >
           <h1>Perspective universitaire</h1>
           <h3>Poursuivre ses études et approfondir ses connaissances.</h3>
         </motion.section>
       </Link>
 
       <Link href="/futur/stages">
-        <motion.section whileHover={{color: "#4F638D"}} whileTap={{scale: 0.9}} className={styles.sectionStage}>
+        <motion.section
+        initial={{color : lesStyles.couleurInitiale}}
+        animate={{color : lesStyles.couleurInitiale}}
+          whileHover={{ color: lesStyles.couleurHover }}
+          whileTap={{ scale: 0.9 }}
+          className={styles.sectionStage}
+        >
           <h1>Stages</h1>
           <h3>Mettre en pratique ses apprentissages.</h3>
         </motion.section>
       </Link>
 
       <Link href="/futur/perspectives-demplois">
-        <motion.section whileHover={{color: "#4F638D"}} whileTap={{scale: 0.9}} className={styles.sectionEmploi}>
+        <motion.section
+        initial={{color : lesStyles.couleurInitiale}}
+        animate={{color : lesStyles.couleurInitiale}}
+          whileHover={{ color: lesStyles.couleurHover }}
+          whileTap={{ scale: 0.9 }}
+          className={styles.sectionEmploi}
+        >
           <h1>Perspectives d'emploi</h1>
           <h3>Devenir un professionnel dans le domaine.</h3>
         </motion.section>
