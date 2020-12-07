28a9ea31aece18e623d3fbc272c14a686a4cdcb6
Olivier Fortier <olivier_fortier@outlook.com>
2020-12-06 22:36:23 -0500

    e

--- pages/introduction.js
+++ pages/introduction.js
@@ -5,10 +5,14 @@ import Head from "next/head";
 import { useEffect, useState } from "react";
 import { useListeThemes, useTheme } from "../hooks/contexteTheme";
 import Canvas3D from "../components/3d/Canvas3D";
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 import Chargement from "../components/Chargement";
 
-const CanvasDynamique = dynamic(() => import("../components/3d/Canvas3D"), {ssr: false, loading: ()=> <Chargement/>});
+// charger le cube 3D dynamiquement pour réduire le temps de chargement de la page
+const CanvasDynamique = dynamic(() => import("../components/3d/Canvas3D"), {
+  ssr: false,
+  loading: () => <Chargement />,
+});
 
 export default function Introduction() {
   // changer couleur selon theme
@@ -74,7 +78,7 @@ export default function Introduction() {
         transition: { ease: "easeInOut", duration: 0.5 },
       }}
       transition={{ duration: 0.75 }}
-      animate={{x:0, y: 0, opacity: 1 }}
+      animate={{ x: 0, y: 0, opacity: 1 }}
       exit={{
         y: -100,
         opacity: 0,
@@ -105,10 +109,11 @@ export default function Introduction() {
       </Head>
 
       <Link href="/cours">
-        <motion.h1 
-        initial={{ opacity: 0, x: -100 }}
-        animate={{ opacity: 1, x: 0, transition: { duration: 0.9 } }}
-        className={styles.headerPage}>
+        <motion.h1
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0, transition: { duration: 0.9 } }}
+          className={styles.headerPage}
+        >
           Envie d'étudier le{" "}
           <motion.b
             animate={{ color: lesStyles.couleurTexte }}
@@ -128,15 +133,21 @@ export default function Introduction() {
         }}
         className={styles.conteneurCube}
       >
-        {/* <Canvas3D classeCanvas={styles.Canvas3D} couleurDuMesh={lesStyles.couleurCube}/> */}
-        {/* <CanvasDynamique classeCanvas={styles.Canvas3D} couleurDuMesh={lesStyles.couleurCube}/> */}
-        <Chargement/>
+        <CanvasDynamique
+          classeCanvas={styles.Canvas3D}
+          couleurDuMesh={lesStyles.couleurCube}
+        />
       </motion.div>
 
-      <motion.p 
-      initial={{ opacity: 0, x: -100 }}
-      animate={{ opacity: 1, x: 0, transition: { duration: 0.9, delay : 0.6} }}
-      className={styles.textePage}>
+      <motion.p
+        initial={{ opacity: 0, x: -100 }}
+        animate={{
+          opacity: 1,
+          x: 0,
+          transition: { duration: 0.9, delay: 0.6 },
+        }}
+        className={styles.textePage}
+      >
         Le TIM c’est l’endroit parfait pour étudier les nouvelles technologies
         de l’industrie. On fait de la programmation, des jeux, de la vidéo, de
         la VR, de la photographie, des sites internets, de l’animation et de la
@@ -154,16 +165,20 @@ export default function Introduction() {
 
       <Link href="/cours">
         <motion.button
-          whileHover={{ scale: 1.05, transition: { duration: 0.3 } , color: lesStyles.couleurHover, borderColor: lesStyles.couleurHover}}
+          whileHover={{
+            scale: 1.05,
+            transition: { duration: 0.3 },
+            color: lesStyles.couleurHover,
+            borderColor: lesStyles.couleurHover,
+          }}
           className={styles.boutonCours}
           style={{
             backgroundColor: lesStyles.couleurBouton,
-           
           }}
-
-          animate={{ color: lesStyles.couleurTexteBouton, borderColor: lesStyles.couleurTexteBouton}}
-
-          
+          animate={{
+            color: lesStyles.couleurTexteBouton,
+            borderColor: lesStyles.couleurTexteBouton,
+          }}
         >
           LISTE DES COURS
         </motion.button>
