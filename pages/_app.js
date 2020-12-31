3b71866e89cb060cff2cd6f2677f44eb1156c7a3
Olivier Fortier <olivier_fortier@outlook.com>
2020-12-31 18:14:05 -0500

    test meilleur fix pour bug transition pages

--- pages/_app.js
+++ pages/_app.js
@@ -6,29 +6,32 @@ import '../styles/styles.scss';
 import { AnimatePresence } from 'framer-motion';
 import Head from 'next/head';
 
-import Router from 'next/router';
+// import Router from 'next/router';
 import { FournisseurTheme } from '../hooks/contexteTheme';
 import Layout from '../components/layout';
+import { useTransitionFix } from '../hooks/useTransitionFix.ts';
 
-const routeChange = () => {
-  // Temporary fix to avoid flash of unstyled content
-  // during route transitions. Keep an eye on this
-  // issue and remove this code when resolved:
-  // https://github.com/vercel/next.js/issues/17464
+// const routeChange = () => {
+//   // Temporary fix to avoid flash of unstyled content
+//   // during route transitions. Keep an eye on this
+//   // issue and remove this code when resolved:
+//   // https://github.com/vercel/next.js/issues/17464
 
-  const tempFix = () => {
-    const allStyleElems = document.querySelectorAll('style[media="x"]');
-    allStyleElems.forEach((elem) => {
-      elem.removeAttribute('media');
-    });
-  };
-  tempFix();
-};
+//   const tempFix = () => {
+//     const allStyleElems = document.querySelectorAll('style[media="x"]');
+//     allStyleElems.forEach((elem) => {
+//       elem.removeAttribute('media');
+//     });
+//   };
+//   tempFix();
+// };
 
-Router.events.on('routeChangeComplete', routeChange);
-Router.events.on('routeChangeStart', routeChange);
+// Router.events.on('routeChangeComplete', routeChange);
+// Router.events.on('routeChangeStart', routeChange);
 
 function MyApp({ Component, pageProps, router }) {
+  const transitionCallback = useTransitionFix();
+
   return (
     <FournisseurTheme>
       <Layout>
